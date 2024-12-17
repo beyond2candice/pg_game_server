57ea0a6a6b70668b6ce693e20958c58f4a45af54
beyond2candice <349855399@qq.com>
2024-12-18 02:04:02 +0800

    修改金额错误问题

--- src/dist/controllers/hip-hop-panda/controller_logic.js
+++ src/dist/controllers/hip-hop-panda/controller_logic.js
@@ -208,7 +208,13 @@ exports.default = {
         const betRate = bet / cartajsonValue.tbb;
         for (var i = step + 1; i < propertyCount; i++) 
         {
-            prevBalance = prevBalance + allfunctions_1.default.MultiplyValueWithFix(cartajson[i].ctw-cartajson[i].tb , betRate);
+            if(i === propertyCount - 1)
+            {
+                prevBalance = prevBalance + allfunctions_1.default.MultiplyValueWithFix(cartajson[i].ctw - cartajsonValue.tb, betRate);
+            }
+            else{
+                prevBalance = prevBalance + allfunctions_1.default.MultiplyValueWithFix(cartajson[i].ctw, betRate);
+            }
         }
         //prevBalance = prevBalance + allfunctions_1.default.MultiplyValueWithFix(cartajsonValue.aw-cartajsonValue.tbb , betRate);
         var lw = null;
